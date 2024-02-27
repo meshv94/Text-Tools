c78acaa4462728dda0bc31fec4ae14b7745ed7b8
meshv <meshvpatel.ics@gmail.com>
2024-02-27 17:01:10 +0530

    push form main

---
+++ build/precache-manifest.e2e022b45b798632f144ae42e340e066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b1097407f9af445382",
+    "url": "/Text%20Utilities/static/css/main.810b8a17.chunk.css"
+  },
+  {
+    "revision": "28b1097407f9af445382",
+    "url": "/Text%20Utilities/static/js/main.28b10974.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/Text%20Utilities/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "84ec5109ea3e66734f44",
+    "url": "/Text%20Utilities/static/js/2.84ec5109.chunk.js"
+  },
+  {
+    "revision": "98c6f01aa21f934e3caf",
+    "url": "/Text%20Utilities/static/js/runtime~main.98c6f01a.js"
+  },
+  {
+    "revision": "f9d40386fdf62597751f3be7d0942ac9",
+    "url": "/Text%20Utilities/index.html"
+  }
+];
